9d7dca16be48e9056c8f794d829ae1bd71deec58
tnangel1984 <tnangel1984123@gmail.com>
2018-04-03 10:27:27 -0400

    connected angular controller. Tested. Working

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,13 @@ console.log("app.js is connected");
 const app = angular.module('codeBreak', []);
 
 // Create controller: takes 2 parameters controllerName and and array. A function must be the last element in the array. call the angular $http module in the function & a variable for it
-// app.controller('codeBreak', ['$http',function($http){
-//
-//         this.getJokes = ()=>{
-//
-//
-//         }
-//
-// }])
+app.controller('codeBreak', ['$http',function($http){
+
+this.test="testing"
+
+        // this.getJokes = ()=>{
+        //
+        //
+        // }
+
+}])
