b4dafc546859e3d8d7179f05e9f31fb1e5ed586a
tnangel1984 <tnangel1984123@gmail.com>
2018-04-03 10:25:07 -0400

    Created and Connected Angular, app.js & Boilerplate Html

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+console.log("app.js is connected");
+
+//create module with name app.
+const app = angular.module('codeBreak', []);
+
+// Create controller: takes 2 parameters controllerName and and array. A function must be the last element in the array. call the angular $http module in the function & a variable for it
+// app.controller('codeBreak', ['$http',function($http){
+//
+//         this.getJokes = ()=>{
+//
+//
+//         }
+//
+// }])
